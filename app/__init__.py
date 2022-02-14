6ec86b79a699aa4e410fc73ae71d07fe9365c3bd
erhic <eric.gichovi@student.moringaschool.com>
2022-02-14 08:32:42 +0300

    setting my app confuguration, the secret key

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,9 @@ from flask import Flask,Blueprint
 from auth import auth
 from quotes import quotes
 
-def create_app():
+def create_app(config_file='settings.py'):
     app=Flask(__name__)
-    
+    app.config.from_pyfile(config_file)
     app.register_blueprint(auth)
     app.register_blueprint(quotes)
     

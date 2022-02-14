ad3b238b29c88e51428a0c3e675602eb85d72764
erhic <eric.gichovi@student.moringaschool.com>
2022-02-14 08:41:11 +0300

    setting up the my database by intializing sqlachemy in the root app

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask,Blueprint
 from auth import auth
 from quotes import quotes
+from .extension import db
 
 def create_app(config_file='settings.py'):
     app=Flask(__name__)
     app.config.from_pyfile(config_file)
+    
+    db.init_app(app)
     app.register_blueprint(auth)
     app.register_blueprint(quotes)
     

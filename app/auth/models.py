a16a2b0c463f204e9942dc8fa0fc8a11b4568ce7
erhic <eric.gichovi@student.moringaschool.com>
2022-02-14 19:22:55 +0300

    set the column in the user database table

--- app/auth/models.py
+++ app/auth/models.py
@@ -1,5 +1,9 @@
 from extension import db
 
 class User(db.Model):
-    pass
+    id=db.Column(db.Integer(),primary_key=True)
+    username=db.Column(db.String(50),nullable=False)
+    email=db.Column(db.String(50),nullable=True,unique=True)
+    password=db.Column(db.String(70),nullable=True)
+    
 
